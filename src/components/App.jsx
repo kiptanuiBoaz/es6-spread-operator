b79041f06ac0f139e53e25eae8e6f9193f1b5489
boaz <boaserem022@gmail.com>
2022-05-29 10:18:10 +0300

    second commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,24 +11,11 @@ function App() {
     const { name, value } = event.target;
 
     setContact(prevValue => {
-      if (name === "fName") {
-        return {
-          fName: value,
-          lName: prevValue.lName,
-          email: prevValue.email
-        };
-      } else if (name === "lName") {
-        return {
-          fName: prevValue.fName,
-          lName: value,
-          email: prevValue.email
-        };
-      } else if (name === "email") {
-        return {
-          fName: prevValue.fName,
-          lName: prevValue.lName,
-          email: value
-        };
+      return{ 
+        ...prevValue,
+       [name]:value
+        //  key value should be written using array syntax
+        
       }
     });
   }
